677fa6e87c5203c70d5e26d9b3ffc5d24438ce8a
yejeungJi <jiyejeung@gmail.com>
2022-05-20 20:17:41 +0900

    ElementTool.js 파일 작성 완료

---
+++ src/javascript/utils/ElementTool.js
@@ -0,0 +1,25 @@
+export const $ = (element) => document.querySelector(element);
+
+export const $$ = (elements) => document.querySelectorAll(elements);
+
+export const createElement = (tagName, tagText = '') => {
+	const element = document.createElement(tagName);
+	element.textContent = tagText;
+
+	return element;
+};
+
+export const combineElement = (elements) => {
+	const fragment = document.createDocumentFragment();
+	elements.forEach((element) => fragment.append(element));
+
+	return fragment;
+};
+
+export const removeElement = (parentSelector, selfSelector) => void document.querySelector(parentSelector).querySelector(selfSelector).remove();
+
+export const removeElements = (parentSelector, selfSelector) =>
+	void document
+		.querySelector(parentSelector)
+		.querySelectorAll(selfSelector)
+		.forEach((element) => element.remove());
