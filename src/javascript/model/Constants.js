62c02addfa3e59d3eb7609905780fe27895931f1
yejeungJi <jiyejeung@gmail.com>
2022-05-20 19:10:59 +0900

    Constants.js 파일 작성 완료

---
+++ src/javascript/model/Constants.js
@@ -0,0 +1,23 @@
+export default Object.freeze({
+	generalMatrixBox: {
+		rowKeyword: 'ROW',
+		colKeyword: 'COL',
+		createKeyword: 'CREATE',
+	},
+	calcMatrixBox: {
+		title: 'Matrix Calculator',
+		author: 'Yejeung Ji, Eunyoung Kim',
+	},
+	modalBox: {
+		warning01: '0 ~ 9 까지의 숫자만을 입력해 주세요.',
+		warning02: '-99 ~ 99 까지의 숫자만을 입력해 주세요.',
+		warning03: '행렬의 덧셈을 위해서는 반드시 두 행렬의 행과 열의 개수가 같아야합니다.',
+		warning04: '행렬의 뺄셈을 위해서는 반드시 두 행렬의 행과 열의 개수가 같아야합니다.',
+		warning05: '행렬의 곱셈을 위해서는 반드시 첫 번째 행렬의 행의 개수와 두 번째 행렬의 열의 개수가 같아야합니다.',
+		warning06: '첫 번째 행렬을 완성하여 주세요.',
+		warning07: '두 번째 행렬을 완성하여 주세요.',
+		warning08: '연산을 위하여 두 행렬을 완성하여 주세요.',
+		warning09: '첫 번째 행렬의 값을 확인하여 주세요.',
+		warning10: '두 번째 행렬의 값을 확인하여 주세요.',
+	},
+});
