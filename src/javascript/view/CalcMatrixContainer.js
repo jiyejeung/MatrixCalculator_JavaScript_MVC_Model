01a466d81a759c5e7a58a4d6c5a47d6f6b52d4bc
yejeungJi <jiyejeung@gmail.com>
2022-05-21 22:22:40 +0900

    CalcMatrixContainer.js 파일 수정 완료

--- src/javascript/view/CalcMatrixContainer.js
+++ src/javascript/view/CalcMatrixContainer.js
@@ -1,4 +1,4 @@
-import { combineElement, createElement } from '../utils/ElementTool.js';
+import { createElement } from '../utils/ElementTool.js';
 
 export default class {
 	constructor(h1CalcMatrixTitleText) {
@@ -11,12 +11,6 @@ export default class {
 		sectionCalcMatrixContainer.append(this.printMatrixTopContainer(), this.printMatrixBottomContainer());
 
 		return sectionCalcMatrixContainer;
-	}
-	printTitle() {
-		const h1CalcMatrixTitle = createElement('H1', this.h1CalcMatrixTitleText);
-		h1CalcMatrixTitle.className = 'h1CalcMatrixTitle';
-
-		return h1CalcMatrixTitle;
 	}
 	printMatrixTopContainer() {
 		const divTopCalcMatrixContainer = createElement('DIV');
@@ -39,10 +33,15 @@ export default class {
 			this.printMatrixButtonContainer('buttonCalcMinus', 'fa-solid fa-square-minus iCalcMinus'),
 			this.printMatrixButtonContainer('buttonCalcMultiply', 'fa-solid fa-square-xmark iCalcMultiply')
 		);
-		
 		divBottomCalcMatrixContainer.append(this.printTitle(), divBottomCalcMatrixButtonContainer);
 
 		return divBottomCalcMatrixContainer;
+	}
+	printTitle() {
+		const h1CalcMatrixTitle = createElement('H1', this.h1CalcMatrixTitleText);
+		h1CalcMatrixTitle.className = 'h1CalcMatrixTitle';
+
+		return h1CalcMatrixTitle;
 	}
 	printMatrixButtonContainer(firstArg, iCalcClassName) {
 		const buttonCalcMatrixContainer = createElement('BUTTON');
